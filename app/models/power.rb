7cd495ebb32374bc39caffe2c7336d50b3ebabe8
Paul Gruson <p.gruson@gmail.com>
2022-07-21 11:43:42 +0200

    fix

--- app/models/power.rb
+++ app/models/power.rb
@@ -5,5 +5,7 @@ class Power < ActiveRecord::Base
   belongs_to :from, polymorphic: true
   belongs_to :to, polymorphic: true
 
+  delegate :name, to: :from, prefix: true, allow_nil: true
+  delegate :name, to: :to, prefix: true, allow_nil: true
 
 end
